25889eb65e820492ed6d8987ed379bf75d252512
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-06-08 14:20:46 -0400

    updated JS and HTML

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// creating buttons and event handlers
+
+// goBtn = document.getElementById("btn"); - start button
+let goBtn = document.getElementById('go-btn');
+goBtn.addEventListener('click', incPage());
+
+// navigateBtn = document.getElementById("next"); - next button
+nextBtn = document.getElementById("next-btn");
+nextBtn.addEventListener('click', incPage());
+
+// resetBtn = document.getElementById("reset-btn"); - reset button
+resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener('click', reset());
+
+// render application
+function renderView() {
+let appMindReader = document.getElementById("doc");
+appMindReader.innerHTML = content;
+
+let content = "Mind reader application. Guess a number between 0-99, and I'll show you the symbol."
+}
+
+let pages = {
+
+}
+
+// functions
+
+let app
+
+function initializeUI(e){
+    renderView(); 
+        setView: 1
+    
+}
+
+function incPage(e){
+    
+}
+
+function resetUI(e){
+
+}
+
+function changeState(e){
+
+}
+
+### Functional ###
+
+START
+FUNCTIONS
+
+
+setState
+
+generateSymbols - 
+
+### Object oriented ###
+
+let mindReader = ()=> {
+
+} 
+
